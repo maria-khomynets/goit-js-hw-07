a9d7bbfc7dc380ce4821eab7c571b13ddb03c524
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-02 16:33:19 +0300

    d

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,11 @@
-const btn1 = document.querySelector('#magic-btn');
-const nav = document.querySelector('.item');
-console.log(nav);
+// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
+
+// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента(тегу < h2 >) і
+// кількість елементів у категорії(усіх < li >, вкладених у нього).
+
+const ulList = document.querySelector('#categories');
+const liList = document.querySelectorAll('.item');
+console.log(ulList);
+
+console.log(liList);
