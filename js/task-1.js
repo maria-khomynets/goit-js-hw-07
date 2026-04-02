37a584d57e487301d03276bdda0946b784110c4a
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-02 16:58:03 +0300

    task1

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента(тегу < h2 >)
+//і кількість елементів у категорії(усіх < li >, вкладених у нього).
+
+const categoryItems = document.querySelectorAll('.item');
+console.log(categoryItems);
+const categories = categoryItems.length;
+console.log(`Number of categories: ${categories}`);
+
+for (let i = 0; i < categoryItems.length; i++) {
+  const title = categoryItems[i].querySelector('h2').textContent;
+  const count = categoryItems[i].querySelectorAll('li').length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${count}`);
+}
