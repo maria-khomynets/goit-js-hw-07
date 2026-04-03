f4626a3419c8238ff78850fb720aafe5aa8a885d
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-03 12:09:22 +0300

    tsak5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// Напиши скрипт, який змінює колір фону елемента < body >
+//   через інлайн - стиль по кліку на button.change - color і
+// задає це значення кольору текстовим вмістом для span.color.
+const buttonClick = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+buttonClick.addEventListener('click', handlClick);
+spanEl.addEventListener('click', handlClick);
+function handlClick(event) {
+  event.target.style.backgroundColor = getRandomHexColor();
+}
